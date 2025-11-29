6510ea340048d6a8884bdac7d805921bbf53b4e4
Martin Skalický <skalicky.martas@seznam.cz>
2025-11-29 13:44:07 +0100

    feat: add big mic icons as buttons to player

--- modules/frontend/src/components/videoStream/video-stream.js
+++ modules/frontend/src/components/videoStream/video-stream.js
@@ -1,5 +1,7 @@
 /* Project https://github.com/AlexxIT/go2rtc */
 import { VideoRTC } from './video-rtc.js';
+const micIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="40" viewBox="0 0 24 24" width="40"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 14c1.66 0 2.99-1.34 2.99-3L15 5c0-1.66-1.34-3-3-3S9 3.34 9 5v6c0 1.66 1.34 3 3 3zm5.3-3c0 3-2.54 5.1-5.3 5.1S6.7 14 6.7 11H5c0 3.41 2.72 6.23 6 6.72V21h2v-3.28c3.28-.48 6-3.3 6-6.72h-1.7z"/></svg>`;
+const micOffIcon = `<svg xmlns="http://www.w3.org/2000/svg" height="40" viewBox="0 0 24 24" width="40"><path d="M0 0h24v24H0zm0 0h24v24H0z" fill="none"/><path d="M19 11h-1.7c0 .74-.16 1.43-.43 2.05l1.23 1.23c.56-.98.9-2.09.9-3.28zm-4.02.17c0-.06.02-.11.02-.17V5c0-1.66-1.34-3-3-3S9 3.34 9 5v.18l5.98 5.99zM4.27 3L3 4.27l6.01 6.01V11c0 1.66 1.33 3 2.99 3 .22 0 .44-.03.65-.08l1.66 1.66c-.71.33-1.5.52-2.31.52-2.76 0-5.3-2.1-5.3-5.1H5c0 3.41 2.72 6.23 6 6.72V21h2v-3.28c.91-.13 1.77-.45 2.54-.9L19.73 21 21 19.73 4.27 3z"/></svg>`
 
 /**
  * This is example, how you can extend VideoRTC player for your app.
@@ -19,11 +21,11 @@ export class VideoStream extends VideoRTC {
         if (!button) return;
 
         if (value) {
-            button.innerText = 'Mluvte';
+            button.innerHTML = micIcon;
             this.unmuteMicrophone()
         }
         else {
-            button.innerText = 'Ztlumeno';
+            button.innerHTML = micOffIcon;
             this.muteMicrophone()
         }
     }
@@ -46,7 +48,7 @@ export class VideoStream extends VideoRTC {
         <style>
         .info {
             position: absolute;
-            top: 25px;
+            top: 15px;
             left: 0;
             padding: 12px;
             color: white;
@@ -55,18 +57,32 @@ export class VideoStream extends VideoRTC {
         }
         .mic {
             position: absolute;
-            top: 25px;
+            top: 5px;
             right: 0;
             padding: 12px;
-            color: white;
             display: flex;
+        }
+        .mic button {
+            background: none;
+            border: none;
+            padding: 8px;
+            border-radius: 50%;
+            transition: background-color 150ms cubic-bezier(0.4, 0, 0.2, 1);
+            cursor: pointer;
+            pointer-events: all;
+        }
+        .mic button:hover {
+            background-color: rgba(255, 255, 255, 0.1);
+        }
+        .mic svg {
+            fill: #fff;
         }
         </style>
         <div class="info">
             <div class="mode"></div>
             <div class="status"></div>
         </div>
-        ${this.isMicrophoneEnabled() ? '<div class="mic"><button>mluvit</button></div>' : ''}
+        ${this.isMicrophoneEnabled() ? `<div class="mic"><button>${micOffIcon}</button></div>` : ''}
         `;
 
         const info = this.querySelector('.info');
